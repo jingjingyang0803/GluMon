87d63c87dbebea4721570a8004143097347e034e
Jingjing Yang <jingjing.yang@tuni.fi>
2025-01-23 11:14:16 +0200

    Make glucose card dynamically changes color based on the glucose level

--- lib/components/current_glucose_card.dart
+++ lib/components/current_glucose_card.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:glu_mon/utils/color_utils.dart';
 
 class CurrentGlucoseCard extends StatelessWidget {
   final String value;
   final String unit;
   final String time;
 
-  const CurrentGlucoseCard(
-      {super.key, required this.value, required this.unit, required this.time});
+  const CurrentGlucoseCard({
+    super.key,
+    required this.value,
+    required this.unit,
+    required this.time,
+  });
+
+  Color getGlucoseColor(double glucose) {
+    if (glucose < 70) {
+      return Colors.redAccent; // Low glucose
+    } else if (glucose >= 70 && glucose <= 140) {
+      return Colors.green; // Normal range
+    } else if (glucose > 140 && glucose <= 180) {
+      return Colors.orangeAccent; // Slightly high
+    } else {
+      return Colors.red; // Very high
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    double glucoseValue =
+        double.tryParse(value) ?? 0.0; // Convert value to double
+
     return Container(
       width: MediaQuery.of(context).size.width * 0.4,
       padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: primaryOrange,
+        color: getGlucoseColor(glucoseValue), // Dynamic color
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
@@ -28,15 +46,19 @@ class CurrentGlucoseCard extends StatelessWidget {
               Text(time, style: TextStyle(color: Colors.white)),
             ],
           ),
-          Row(children: [
-            Text(value,
+          Row(
+            children: [
+              Text(
+                value,
                 style: TextStyle(
                     fontSize: 28,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            SizedBox(width: 8),
-            Text(unit, style: TextStyle(color: Colors.white)),
-          ]),
+                    color: Colors.white),
+              ),
+              SizedBox(width: 8),
+              Text(unit, style: TextStyle(color: Colors.white)),
+            ],
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:glu_mon/screens/settings_page.dart';
+import 'package:glu_mon/screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,9 +32,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: HomePage(),
+      home: HomePage(),
       // home: TrendPage(),
-      home: SettingsPage(),
+      // home: SettingsPage(),
     );
   }
 }
