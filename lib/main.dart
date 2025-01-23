20e5b04b8a72ee35cf713e2a8e7a2726046f60b0
Jingjing Yang <jingjing.yang@tuni.fi>
2025-01-23 11:54:37 +0200

    Apply background color bands to line chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:glu_mon/screens/home_page.dart';
+import 'package:glu_mon/screens/trend_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,8 +32,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
-      // home: TrendPage(),
+      // home: HomePage(),
+      home: TrendPage(),
       // home: SettingsPage(),
     );
   }
